7a70447232dfc8cb3c2379fdf745994eb75153c6
VivekAlhat <vivekalhat14@outlook.com>
2021-07-29 23:59:25 +0530

    Setup Project Repo | Add Deps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Global, css } from "@emotion/react";
+import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import "focus-visible/dist/focus-visible";
+import "./index.css";
+
+const GlobalStyles = css`
+  .js-focus-visible :focus:not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <Global styles={GlobalStyles} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
