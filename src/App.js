52f701c4c587849400e6ddb3249a5179fada4377
VivekAlhat <vivekalhat14@outlook.com>
2021-08-04 16:07:14 +0530

    Create Higher Order Component To Redirect Users

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import Dashboard from "./pages/Dashboard";
 import PasswordReset from "./pages/PasswordReset";
+import ProfileRedirect from "./HOC/ProfileRedirect";
 import { Switch, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { useLocation } from "react-router-dom";
@@ -19,12 +20,12 @@ function App() {
       <Navbar />
       <AnimatePresence exitBeforeEnter initial={false}>
         <Switch location={location} key={location.pathname}>
-          <Route exact path="/" component={Home} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/signin" component={Signin} />
+          <ProfileRedirect exact path="/" component={Home} />
+          <ProfileRedirect path="/signup" component={Signup} />
+          <ProfileRedirect path="/signin" component={Signin} />
           <Route path="/profile" component={Profile} />
           <Route path="/dashboard" component={Dashboard} />
-          <Route path="/reset" component={PasswordReset} />
+          <ProfileRedirect path="/reset" component={PasswordReset} />
           <Route path="*" component={Error} />
         </Switch>
       </AnimatePresence>
---
+++ src/HOC/ProfileRedirect.jsx
@@ -0,0 +1,20 @@
+import { Route, Redirect } from "react-router-dom";
+import { useSession } from "../hooks/useSession";
+
+const ProfileRedirect = ({ component: Component, ...props }) => {
+  const { user } = useSession();
+  return (
+    <Route
+      {...props}
+      render={(props) =>
+        !user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/dashboard" }} from={props.location} />
+        )
+      }
+    />
+  );
+};
+
+export default ProfileRedirect;
