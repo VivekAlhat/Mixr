12983a6212c8e839e5fe103a45feac078a0ac705
VivekAlhat <vivekalhat14@outlook.com>
2021-08-04 15:57:33 +0530

    Modify Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { FaSun, FaMoon } from "react-icons/fa";
+import { Link as RouteLink } from "react-router-dom";
 import {
+  Text,
   Flex,
   IconButton,
   Spacer,
@@ -13,9 +15,21 @@ const Navbar = () => {
   const isDark = colorMode === "dark";
 
   return (
-    <Flex w="100%" pr={notSmallerScreen ? "10" : "5"} pt="5">
+    <Flex w="100%" boxShadow="sm" mb="5" p="1">
+      <Text
+        as={RouteLink}
+        to="/"
+        fontSize="2xl"
+        fontWeight="bold"
+        pl="5"
+        color={isDark ? "blue.300" : "blue.700"}
+      >
+        Mixr
+      </Text>
       <Spacer />
       <IconButton
+        alignSelf="center"
+        mr={notSmallerScreen ? "10" : "5"}
         variant="ghost"
         icon={isDark ? <FaSun /> : <FaMoon />}
         onClick={toggleColorMode}
